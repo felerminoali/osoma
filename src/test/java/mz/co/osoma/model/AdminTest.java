270072e8d1274f7af93af73a5eb1e9aef45de886
EltonUG <eltonug@gmail.com>
2019-07-04 01:30:49 -0700

    Tests bug

--- src/test/java/mz/co/osoma/model/AdminTest.java
+++ src/test/java/mz/co/osoma/model/AdminTest.java
@@ -1,9 +1,17 @@
 package mz.co.osoma.model;
 
+import mz.co.osoma.config.HibernateConf;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = HibernateConf.class)
 public class AdminTest {
 
     @Test
