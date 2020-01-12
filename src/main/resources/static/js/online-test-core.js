3e80a181cbca84de7e6af26499c79bd7940bfb84
felerminoali <felerminoali@gmail.com>
2020-01-12 11:41:19 +0200

    mat variant upgraded

--- src/main/resources/static/js/online-test-core.js
+++ src/main/resources/static/js/online-test-core.js
@@ -32,9 +32,9 @@ $(document).ready(function () {
     function initComponents() {
 
 
-        // $(document).ajaxSend(function () {
-        //     $("#overlay").fadeIn(300);
-        // });
+        $(document).ajaxSend(function () {
+            $("#overlay").fadeIn(300);
+        });
 
 
         $('#qnext').on('click', function (e) {
@@ -292,106 +292,124 @@ $(document).ready(function () {
     function fetchQuestions(pointer) {
 
         var data = $("#my").data("all");
-        //var examID = $('#examid').val();
-        var index = parseInt($('#index').val()) + pointer;
 
 
-        $("#index").val(index);
-        $("#q_index").html(index + 1);
+        $.ajax(this.href, {
+            success: function() {
+
+                var index = parseInt($('#index').val()) + pointer;
+
+                $("#index").val(index);
+                $("#q_index").html(index + 1);
 
 
-        var media_img = '';
-        if (data.questionList[index].image != null) {
-            var media_img = '<a href="#"><img class="media-object" src="' + data.questionList[index].image + '" alt="questions.image"></a>';
-        }
+                var media_img = '';
+                if (data.questionList[index].image != null) {
+                    var media_img = '<a href="#"><img class="media-object" src="' + data.questionList[index].image + '" alt="questions.image"></a>';
+                }
 
-        $('#q_img').html(media_img);
+                $('#q_img').html(media_img);
 
-        var q_question = document.getElementById("q_question");
-        q_question.innerHTML = data.questionList[index].question;
-        MathJax.Hub.Queue(["Typeset", MathJax.Hub, q_question]);
+                var q_question = document.getElementById("q_question");
+                q_question.innerHTML = data.questionList[index].question;
+                MathJax.Hub.Queue(["Typeset", MathJax.Hub, q_question]);
 
-        var htmlCaseOfStudy = data.questionList[index].caseofstudy;
-        var shortText = '';
-        if (htmlCaseOfStudy !== null) {
-            var noHtmlCaseOfStudy = $(htmlCaseOfStudy);
-            shortText = noHtmlCaseOfStudy.text().substring(0, 120);
-            $('#q_caseofstudy').html(data.questionList[index].caseofstudy);
-            shortText += '&nbsp;...&nbsp;' + '<a href="#" data-toggle="modal" data-target="#myModal">Monstrar mais</a>';
-        }
+                var htmlCaseOfStudy = data.questionList[index].caseofstudy;
+                var shortText = '';
+                if (htmlCaseOfStudy !== null) {
+                    var noHtmlCaseOfStudy = $(htmlCaseOfStudy);
+                    shortText = noHtmlCaseOfStudy.text().substring(0, 120);
+                    $('#q_caseofstudy').html(data.questionList[index].caseofstudy);
+                    shortText += '&nbsp;...&nbsp;' + '<a href="#" data-toggle="modal" data-target="#myModal">Monstrar mais</a>';
+                }
 
-        $('.casestudy').html(shortText);
+                $('.casestudy').html(shortText);
 
-        var str = '<table width="100%">';
+                var str = '<table width="100%">';
 
-        var q_response = '';
-        for (var i = 0; i < data.questionList.length; i++) {
-            q_response += '<a href="#" class="list-group-item answer_index" rel="' + i + '">';
-            q_response += '<span class="badge pull-left">' + (i + 1) + '</span>&nbsp;';
-            var session_saved_choice = getAnswerById(data.questionList[i].id);
+                var q_response = '';
+                for (var i = 0; i < data.questionList.length; i++) {
+                    q_response += '<a href="#" class="list-group-item answer_index" rel="' + i + '">';
+                    q_response += '<span class="badge pull-left">' + (i + 1) + '</span>&nbsp;';
+                    var session_saved_choice = getAnswerById(data.questionList[i].id);
 
-            var label = '';
-            if (session_saved_choice['label'] != null) {
-                label = session_saved_choice['label'];
+                    var label = '';
+                    if (session_saved_choice['label'] != null) {
+                        label = session_saved_choice['label'];
+                    }
+                    q_response += '<span id="answer_' + data.questionList[i].id + '">' + label + '</span>';
+                    q_response += '</a>';
+                }
+
+                $(".q_response").html(q_response);
+
+
+                for (var i = 0; i < data.questionList[index].choiceList.length; i++) {
+                    var obj = data.questionList[index].choiceList[i];
+
+                    str += '<div class="media">';
+                    str += '<div class="media-body">';
+                    str += '<div class="clear"></div>';
+                    str += '<div class="ans_select">';
+
+                    var choosed = getAnswerById(data.questionList[index].id);
+                    if (choosed['idChoice'] == obj.id && choosed['idChoice'] != null) {
+                        str += '<input type="radio" id="q_' + data.questionList[index].id + '_' + obj.id + '_' + String.fromCharCode(65 + i) + '" name="q_choice" checked/>';
+                    } else {
+                        str += '<input type="radio" id="q_' + data.questionList[index].id + '_' + obj.id + '_' + String.fromCharCode(65 + i) + '" name="q_choice"/>';
+                    }
+
+                    str += '</div>';
+                    str += '<div class="ans_content editor">';
+                    str += '<label for="q_' + data.questionList[index].id + '_' + obj.id + '_' + String.fromCharCode(65 + i) + '">';
+                    str += '<span id="' + obj.id + '" rel="' + String.fromCharCode(65 + i) + '">' + String.fromCharCode(65 + i) + '.&nbsp;' + obj.answer + '</span>';
+                    str += '</label>';
+                    str += '</div>';
+                    str += '</div>';
+                    str += '</div>';
+                }
+
+                str += '</table>';
+
+                var choicesDiv = document.getElementById("choicesDiv");
+                choicesDiv.innerHTML = str;
+                MathJax.Hub.Queue(["Typeset", MathJax.Hub, choicesDiv]);
+
+                // $(".answersDiv").html(str);
+
+                var btn_prev = '';
+
+                if (index !== 0) {
+                    btn_prev += '<a id="qprev" class="btn btn-danger prev"><i class="glyphicon glyphicon-chevron-left"></i> Anterior</a>';
+                }
+                $('.prevbox').html(btn_prev);
+
+                var div_next = '';
+                if (data.questionList.length - 1 !== index) {
+                    div_next += '<a id="qnext" class="btn btn-danger next">Seguinte<i class="glyphicon glyphicon-chevron-right"></i></a>&nbsp;';
+                }
+                div_next += '<a href="#" id="qfinish" class="btn btn-success t_finish">Terminar<i class="glyphicon glyphicon-chevron-up"></i></a>&nbsp;';
+                div_next += '<a href="#" class="btn btn-info showlist" id="btnShow">Respostas</a>&nbsp;';
+                $('.nextbox').html(div_next);
+
+                getAnswerById(data.questionList[index].id);
+
+                initComponents();
+            },
+            error: function (xhr, textStatus, error) {
+                // alert('An error has occurred ::from save answer ajax call --> ' + error);
+                console.log(xhr.statusText);
+                console.log(textStatus);
+                console.log(error);
             }
-            q_response += '<span id="answer_' + data.questionList[i].id + '">' + label + '</span>';
-            q_response += '</a>';
-        }
+        }).done(function () {
+            setTimeout(function () {
+                $("#overlay").fadeOut(300);
+            }, 200);
+        });
 
-        $(".q_response").html(q_response);
 
 
-        for (var i = 0; i < data.questionList[index].choiceList.length; i++) {
-            var obj = data.questionList[index].choiceList[i];
-
-            str += '<div class="media">';
-            str += '<div class="media-body">';
-            str += '<div class="clear"></div>';
-            str += '<div class="ans_select">';
-
-            var choosed = getAnswerById(data.questionList[index].id);
-            if (choosed['idChoice'] == obj.id && choosed['idChoice'] != null) {
-                str += '<input type="radio" id="q_' + data.questionList[index].id + '_' + obj.id + '_' + String.fromCharCode(65 + i) + '" name="q_choice" checked/>';
-            } else {
-                str += '<input type="radio" id="q_' + data.questionList[index].id + '_' + obj.id + '_' + String.fromCharCode(65 + i) + '" name="q_choice"/>';
-            }
-
-            str += '</div>';
-            str += '<div class="ans_content editor">';
-            str += '<label for="q_' + data.questionList[index].id + '_' + obj.id + '_' + String.fromCharCode(65 + i) + '">';
-            str += '<span id="' + obj.id + '" rel="' + String.fromCharCode(65 + i) + '">' + String.fromCharCode(65 + i) + '.&nbsp;' + obj.answer + '</span>';
-            str += '</label>';
-            str += '</div>';
-            str += '</div>';
-            str += '</div>';
-        }
-
-        str += '</table>';
-
-        var choicesDiv = document.getElementById("choicesDiv");
-        choicesDiv.innerHTML = str;
-        MathJax.Hub.Queue(["Typeset", MathJax.Hub, choicesDiv]);
-
-        // $(".answersDiv").html(str);
-
-        var btn_prev = '';
-
-        if (index !== 0) {
-            btn_prev += '<a id="qprev" class="btn btn-danger prev"><i class="glyphicon glyphicon-chevron-left"></i> Anterior</a>';
-        }
-        $('.prevbox').html(btn_prev);
-
-        var div_next = '';
-        if (data.questionList.length - 1 !== index) {
-            div_next += '<a id="qnext" class="btn btn-danger next">Seguinte<i class="glyphicon glyphicon-chevron-right"></i></a>&nbsp;';
-        }
-        div_next += '<a href="#" id="qfinish" class="btn btn-success t_finish">Terminar<i class="glyphicon glyphicon-chevron-up"></i></a>&nbsp;';
-        div_next += '<a href="#" class="btn btn-info showlist" id="btnShow">Respostas</a>&nbsp;';
-        $('.nextbox').html(div_next);
-
-        getAnswerById(data.questionList[index].id);
-
-        initComponents();
     }
 
 
